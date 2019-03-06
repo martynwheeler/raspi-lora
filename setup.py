4cb6b0520b1fef939dd9ddb85ad0c323e0daf1d1
Anne Wood <anne.w@fastmail.us>
2019-03-05 22:40:12 -0600

    0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='raspi-lora',
-    version='0.1',
+    version='0.2',
     packages=['raspi_lora'],
     url='https://gitlab.com/the-plant/raspi-lora',
     license='MIT',
